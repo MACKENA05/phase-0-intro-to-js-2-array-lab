27000b97fc8a655a3a10f77dc2eb818f348d461d
MonicahMackena¨ <mackenamuthui@gmail.com>
2024-12-08 18:46:27 +0300

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution here!
+const cats = ["Milo", "Otis","Garfield"];
+
+const destructivelyAppendCat =() => {
+    cats.push("Ralph")
+}
+
+const destructivelyPrependCat = () => {
+    cats.unshift("Bob")
+};
+
+const destructivelyRemoveLastCat = () => {
+    cats.pop();
+}
+
+const destructivelyRemoveFirstCat = () => {
+    cats.shift();
+};
+
+const appendCat = (name) => {
+  return [...cats, "Broom"];
+}
+ const prependCat = (name) => {
+    return ["Arnold", ...cats];
+ }
+const removeLastCat = () => {
+    return cats.slice(0,2);
+}
+
+const removeFirstCat = () => {
+    return cats.slice(-2);
+}
